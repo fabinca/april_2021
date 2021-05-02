ffa9c575c8e19f3edf8d0e167b5e504aef778cae
Me <ca.fabian@gmx.de>
2021-05-02 22:51:39 +0200

    starting recursive

--- C05_first/ex08/ft_ten_queens_puzzle.c
+++ C05_first/ex08/ft_ten_queens_puzzle.c
@@ -20,7 +20,7 @@ void initialize(char *array, int len)
 	i = 0;
 	while (i < len)
 	{
-		array[i] = 'b';
+		array[i] = '.';
 		i++;
 	}
 	array[i] = 0;
@@ -32,10 +32,10 @@ void print_solution(char row[10])
 	int i;
 
 	i = 0;
-	while (row[i])   //f.e. "abcdefghij"
+	while (i < 10)   //f.e. "abcdefghij"
 	{
-		//print_it = row[i] - 49;
-		write(1, &row[i], 1);
+		print_it = row[i] - 17;
+		write(1, &print_it, 1);
 		i++;
 	}
 	write(1, "\n", 1);
@@ -43,9 +43,27 @@ void print_solution(char row[10])
 
 void solve_recursive(int col, char row[10], char dia_left[19], char dia_right[19])
 {
+	char next_row;
+
+	next_row = 'A';
 	if (col < 10)
 	{
-		//put_next_queen(col + 1, lines_cp);
+		printf("col: %d\n", col);
+		printf("dia_right: %s\n", dia_right);
+		printf("dia_left: %s\n", dia_left);
+		printf("row: %s\n", row);
+		while (next_row <= 'J')
+		{
+			if (char_in_array(next_row, row))
+				continue;
+			else if (char_in_array(, dia_left))
+				continue;
+			else if (char_in_array(, dia_left))
+				continue;
+			else
+			
+		}
+		
 	}
 	else
 		print_solution(row);
@@ -60,12 +78,9 @@ int	ft_ten_queens_puzzle(void)
 
 	col = 10;
 	initialize(row, 10);
-	printf("row: %s\n", row);
 	initialize(dia_left, 19);
-	printf("dia_left: %s\n", dia_left);
 	initialize(dia_right, 19);
-	printf("dia_right: %s\n", dia_right);
 	print_solution(row);
-	//solve_recursive(col, row, dia_left, dia_right);
+	solve_recursive(col, row, dia_left, dia_right);
 	return (0);
 }
