29fb295d5b25d547d5cf3979a586e2edc53057ff
Me <ca.fabian@gmx.de>
2021-05-02 19:14:29 +0200

    pointer to pointer added

--- C05_first/ex08/ft_ten_queens_puzzle.c
+++ C05_first/ex08/ft_ten_queens_puzzle.c
@@ -12,6 +12,7 @@
 
 #include <unistd.h>
 #include <malloc.h>
+#include <stdio.h>
 
 void initialize(char *array, int len)
 {
@@ -25,24 +26,62 @@ void initialize(char *array, int len)
 	}
 }
 
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+
+void solve(int col, char **lines, int n)
+{
+	char *row_cp;
+	char *dia_left_cp;
+	char *dia_right_cp;
+	char **lines_cp;
+
+	row_cp = (char*)malloc(n + 1);
+	dia_right_cp = (char*)malloc(2 * n);
+	dia_left_cp = (char*)malloc(2 * n);
+	lines_cp = (char**)malloc(3 + 1);
+	*row_cp = *lines[0];
+	lines_cp[0] = row_cp;
+	lines_cp[1] = dia_left_cp;
+	lines_cp[2] = dia_right_cp;
+	printf("%s\n%s", lines[0], row_cp);
+	free(row_cp);
+	free(dia_right_cp);
+	free(dia_left_cp);
+	free(lines_cp);
+}
+
 int	ft_ten_queens_puzzle(int n)
 {
 	char *row;
 	char *dia_left;
 	char *dia_right;
+	char **lines;
 	int col;
 
 	col = 0;
 	row = (char*)malloc(n + 1);
 	dia_right = (char*)malloc(2 * n);
 	dia_left = (char*)malloc(2 * n);
+	lines = (char**)malloc(3 + 1);
+	lines[0] = row;
+	lines[1] = dia_left;
+	lines[2] = dia_right;
 	initialize(row, n);
 	initialize(dia_left, 2 * n - 1);
 	initialize(dia_right, 2 * n - 1);
-	while (col < n)
-	{
-		//put_next_queen(row);
-		col++;
-	}
+	solve(col, lines, n);
 	return (0);
 }
