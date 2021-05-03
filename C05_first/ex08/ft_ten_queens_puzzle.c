a6d2ff17ea84d84266c25859fe1395ea16293d2e
Me <ca.fabian@gmx.de>
2021-05-03 10:52:16 +0200

    lines pointer added

--- C05_first/ex08/ft_ten_queens_puzzle.c
+++ C05_first/ex08/ft_ten_queens_puzzle.c
@@ -55,7 +55,7 @@ int char_not_in_array(char letter, char array[11], int col)
 	return (1);
 }
 
-void solve_recursive(int col, char row[11], char dia_left[11], char dia_right[11])
+void solve_recursive(int col, char *lines[3])
 {
 	char next_row;
 
@@ -64,19 +64,19 @@ void solve_recursive(int col, char row[11], char dia_left[11], char dia_right[11
 	{
 		while (next_row <= 'J')
 		{
-			if (char_not_in_array(next_row, row, col) && char_not_in_array(next_row + col, dia_left, col) &&
-			 char_not_in_array(next_row + 9 - col, dia_right, col))
+			if (char_not_in_array(next_row, lines[0], col) && char_not_in_array(next_row + col, lines[1], col) &&
+			 char_not_in_array(next_row + 9 - col, lines[2], col))
 			{
-				row[col] = next_row;
-				dia_left[col] = next_row + col;
-				dia_right[col] = next_row + 9 - col;
-				solve_recursive(col + 1, row, dia_left, dia_right);
+				lines[0][col] = next_row;
+				lines[1][col] = next_row + col;
+				lines[2][col] = next_row + 9 - col;
+				solve_recursive(col + 1, lines);
 			}
 			next_row++;
 		}
 	}
 	else
-		print_solution(row);
+		print_solution(lines[0]);
 }
 
 int	ft_ten_queens_puzzle(void)
@@ -84,12 +84,16 @@ int	ft_ten_queens_puzzle(void)
 	char row[11];
 	char dia_left[11];
 	char dia_right[11];
+	char *lines[3];
 	int col;
 
 	col = 0;
-	initialize(row);
-	initialize(dia_left);
-	initialize(dia_right);
-	solve_recursive(col, row, dia_left, dia_right);
+	lines[0] = row;
+	lines[1] = dia_left;
+	lines[2] = dia_right;
+	//initialize(row);
+	//initialize(dia_left);
+	//initialize(dia_right);
+	solve_recursive(col, lines);
 	return (0);
 }
