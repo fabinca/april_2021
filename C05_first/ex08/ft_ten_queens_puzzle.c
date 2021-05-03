2423296e37e4796203a8964d6e8f77a6d541c656
Me <ca.fabian@gmx.de>
2021-05-03 10:20:58 +0200

    solved

--- C05_first/ex08/ft_ten_queens_puzzle.c
+++ C05_first/ex08/ft_ten_queens_puzzle.c
@@ -13,7 +13,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void initialize(char array[10])
+void initialize(char array[11])
 {
 	int i;
 
@@ -26,13 +26,13 @@ void initialize(char array[10])
 	array[i] = 0;
 }
 
-void print_solution(char row[10])
+void print_solution(char row[11])
 {
 	char print_it;
 	int i;
 
 	i = 0;
-	while (i < 10)   //f.e. "abcdefghij"
+	while (i < 5)   //f.e. "abcdefghij"
 	{
 		print_it = row[i] - 17;
 		write(1, &print_it, 1);
@@ -41,7 +41,7 @@ void print_solution(char row[10])
 	write(1, "\n", 1);
 }
 
-int char_not_in_array(char letter, char array[10], int col)
+int char_not_in_array(char letter, char array[11], int col)
 {
 	int i;
 
@@ -55,21 +55,17 @@ int char_not_in_array(char letter, char array[10], int col)
 	return (1);
 }
 
-void solve_recursive(int col, char row[10], char dia_left[10], char dia_right[10])
+void solve_recursive(int col, char row[11], char dia_left[11], char dia_right[11])
 {
 	char next_row;
 
 	next_row = 'A';
 	if (col < 10)
 	{
-		/*printf("col: %d\n", col);
-		printf("dia_right: %s\n", dia_right);
-		printf("dia_left: %s\n", dia_left);
-		printf("row: %s\n", row);*/
 		while (next_row <= 'J')
 		{
 			if (char_not_in_array(next_row, row, col) && char_not_in_array(next_row + col, dia_left, col) &&
-			 char_not_in_array(next_row + 9 - col, dia_left, col))
+			 char_not_in_array(next_row + 9 - col, dia_right, col))
 			{
 				row[col] = next_row;
 				dia_left[col] = next_row + col;
@@ -85,16 +81,15 @@ void solve_recursive(int col, char row[10], char dia_left[10], char dia_right[10
 
 int	ft_ten_queens_puzzle(void)
 {
-	char row[10];
-	char dia_left[10];
-	char dia_right[10];
+	char row[11];
+	char dia_left[11];
+	char dia_right[11];
 	int col;
 
 	col = 0;
 	initialize(row);
 	initialize(dia_left);
 	initialize(dia_right);
-	print_solution(row);
 	solve_recursive(col, row, dia_left, dia_right);
 	return (0);
 }
