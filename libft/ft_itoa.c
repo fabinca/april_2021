ab93537bea3f8432347491a15d794a5c818ca241
Me <ca.fabian@gmx.de>
2021-05-11 19:33:16 +0200

    allocation error handling

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -10,6 +10,11 @@ int     how_many_digits(int n)
     int digits;
 
     digits = 1;
+    if (n < 0)
+    {
+        digits++;
+        n = -n;
+    }
     while (n >= 10)
     {
         digits++;
@@ -25,41 +30,34 @@ void    putnbr_to_str(int n, char *str, int i)
         putnbr_to_str(n / 10, str, i - 1);
         putnbr_to_str(n % 10, str, i);
     }
+    else if (n >= 0)
+        str[i] = n + '0';
     else
     {
-        str[i] = n + '0';
+        str[0] = '-';
+        putnbr_to_str(-n / 10, str, i - 1);
+        putnbr_to_str(-n % 10, str, i);
+        
     }
 }
 
 char    *ft_itoa(int n)
 {
     char    *str;
-    int     i;
-    int     isneg;
     int     strlen;
 
     if (n == -2147483648)
     {
         str = (char*)malloc(12);
+        if (str == NULL)
+            return (NULL);
         str = "-2147483648";
         return (str);
     }
-    i = 0;
-    strlen = 0;
-    isneg = 0;
-    if (n < 0)
-    {
-        isneg = 1;
-        strlen++;
-        n = -n;
-    }
-    strlen += how_many_digits(n);
+    strlen = how_many_digits(n);
     str = (char*)malloc(strlen + 1);
-    if (isneg)
-    {
-        str[i] = '-';
-        i++;
-    }
+    if (str == NULL)
+        return (NULL);
     putnbr_to_str(n, str, strlen - 1);
     str[strlen] = 0;
     return (str);
