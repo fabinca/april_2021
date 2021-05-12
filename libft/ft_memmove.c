5f278cb846e3d29ba1bce708baed26d92dd25935
Me <ca.fabian@gmx.de>
2021-05-12 12:16:18 +0200

    memmove2

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -7,7 +7,7 @@
 /* The memmove() function returns a pointer to dest. */
 #include <unistd.h>
 
-void *memmove(void *dest, const void *src, size_t n)
+void *ft_memmove(void *dest, const void *src, size_t n)
 {
     size_t i;
     void *temp;
